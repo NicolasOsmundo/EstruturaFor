9ee1d2b7ea37b0408df9607c7395dcb058fcfe6d
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 13:39:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class EstruturaFor
+{
+    static void Main(string[] args)
+    {
+
+        // Contando de 0 a 100
+        for(int i = 0; i < 101; i += 1)
+            {
+            Console.WriteLine(i);
+        }
+        Console.Read();
+    }
+
+}
